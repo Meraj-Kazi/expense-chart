7beb8572a61e6e14fa3b31bce1f2a1f6ae717cb0
Meraj Kazi <kazimeraj11@gmail.com>
2023-11-21 20:33:17 +0600

    Make chart as a separate component

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 const Chart = () => {
+    const styles = () => ({
+        width: `16.5rem`,
+        height: `16.5rem`,
+        background: `conic-gradient( #9d9bf4 60deg,
+        #4c49ed 60deg 150deg,
+        #4fd18b 150deg 200deg,
+        #141197 200deg 360deg
+    )`,
+    });
     return (
-        <div>
-            <div></div>
+        <div
+            style={styles()}
+            className=' rounded-[20rem] flex items-center justify-center mb-[2.94rem]'
+        >
+            <div
+                className='bg-[#FFF] w-[13.25rem] h-[13.25rem] rounded-[20rem]
+                            flex items-center justify-center'
+            >
+                <p className=' text-[#111] text-[2.5rem] font-[700]'>
+                    $ 5,485
+                    <span
+                        className={`${inter.className} font-[800] text-[#AFADFE] text-[1.1875rem]`}
+                    >
+                        .00
+                    </span>
+                </p>
+            </div>
         </div>
     );
 };
